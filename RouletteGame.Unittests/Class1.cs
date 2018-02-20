a802918c23d4bc80d3a7dd370f801ff289471816
jonasna <jonasna93@gmail.com>
2018-02-20 14:26:44 +0100

    Add project files.

---
+++ RouletteGame.Unittests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteGame.Unittests
+{
+    public class Class1
+    {
+    }
+}
